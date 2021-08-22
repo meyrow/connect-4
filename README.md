9fa7ba3c62f76e02b819b20d820ffb400ac3414b
Ilan Meyrowitsch <meyrowitsch@gmail.com>
2021-08-22 13:48:37 +0300

    upload project commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# mastering connect-4 game with Alpha Zero General
+This project is based on https://github.com/suragnair/alpha-zero-general
+For my studies I have tried the alpha-zero-general with connect-4 game.
+The purpose was to see if the trained models would draw after training.
+The code in this project is a skeleton of the original alpha-zero-general. 
+I have added printout inorder to calculate results of trained models validation errors and investigate improvements in self-play games.
+I will edit this readme with the outcome of me research.
+
+```bash
+python main.py
+```
+If you are interested in running this project with other parameters then modify ```main.py```.
+
+### Experiments
+I trained a PyTorch model for Connect-5 (~100 iterations, 50 episodes per iteration and 15 MCTS simulations per turn). This took about 12 hours. 
+
+Description of relevant algorithm can be found 
+1.
+[Hyper-Parameter Sweep on AlphaZero General](https://arxiv.org/pdf/1903.08129.pdf).
+2.
+[Mastering the game of Go with deep neural networks and tree search](https://www.nature.com/articles/nature16961).
+3.
+[Learning to Play Othello Without Human Knowledge](https://github.com/suragnair/alpha-zero-general/raw/master/pretrained_models/writeup.pdf).
+
+
---
+++ main.py
@@ -0,0 +1,62 @@
+import logging
+
+import coloredlogs
+
+from Coach import Coach
+from connect4.Connect4Game import Connect4Game
+from connect4.pytorch.NNet import NNetWrapper as nn
+from utils import dotdict
+
+
+logging.basicConfig(filename='C:/ilan/python/connect4.log',
+                        filemode='a',
+                        format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                        datefmt='%H:%M:%S',
+                        level=logging.INFO)
+log = logging.getLogger(__name__)
+coloredlogs.install(level='INFO')  # Change this to DEBUG to see more info.
+
+args = dotdict({
+    'numIters': 100,
+    'numEps': 50,              # 50 Number of complete self-play games to simulate during a new iteration.
+    'tempThreshold': 10,        # 15
+    'updateThreshold': 0.6,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
+    'maxlenOfQueue': 20000,    # 20000 Number of game examples to train the neural networks.
+    'numMCTSSims': 15,          # 15 Number of games moves for MCTS to simulate.
+    'arenaCompare': 40,         # 40 Number of games to play during arena play to determine if new net will be accepted.
+    'cpuct': 1,
+
+    'checkpoint': './temp/',
+    'load_model': False,
+    'load_folder_file': ('/dev/models/8x100x50','best.pth.tar'),
+    'numItersForTrainExamplesHistory': 20,
+
+})
+
+
+def main():
+    log.info('Loading %s...', Connect4Game.__name__)
+    g = Connect4Game()
+
+    log.info('Loading %s...', nn.__name__)
+    nnet = nn(g)
+
+    if args.load_model:
+        log.info('Loading checkpoint "%s/%s"...', args.load_folder_file)
+        nnet.load_checkpoint(args.load_folder_file[0], args.load_folder_file[1])
+    else:
+        log.warning('Not loading a checkpoint!')
+
+    log.info('Loading the Coach...')
+    c = Coach(g, nnet, args)
+
+    if args.load_model:
+        log.info("Loading 'trainExamples' from file...")
+        c.loadTrainExamples()
+
+    log.info('Starting the learning process ')
+    c.learn()
+
+
+if __name__ == "__main__":
+    main()
