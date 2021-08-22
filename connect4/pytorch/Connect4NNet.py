0266ed3aa97dfb5e958efaf4c8c4d569a1bda7a3
Ilan Meyrowitsch <meyrowitsch@gmail.com>
2021-08-22 15:47:55 +0300

    descriptions

--- connect4/pytorch/Connect4NNet.py
+++ connect4/pytorch/Connect4NNet.py
@@ -21,12 +21,10 @@ class Connect4NNet(nn.Module):
         self.conv1 = nn.Conv2d(1, args.num_channels, 3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(args.num_channels, args.num_channels, 3, stride=1, padding=1)
         self.conv3 = nn.Conv2d(args.num_channels, args.num_channels, 3, stride=1)
-        # self.conv4 = nn.Conv2d(args.num_channels, args.num_channels, 3, stride=1)
 
         self.bn1 = nn.BatchNorm2d(args.num_channels)
         self.bn2 = nn.BatchNorm2d(args.num_channels)
         self.bn3 = nn.BatchNorm2d(args.num_channels)
-        # self.bn4 = nn.BatchNorm2d(args.num_channels)
 
         self.fc1 = nn.Linear(args.num_channels*(self.board_x-2)*(self.board_y-2), 512)
         self.fc_bn1 = nn.BatchNorm1d(512)
@@ -44,7 +42,6 @@ class Connect4NNet(nn.Module):
         s = F.relu(self.bn1(self.conv1(s)))                          # batch_size x num_channels x board_x x board_y
         s = F.relu(self.bn2(self.conv2(s)))                          # batch_size x num_channels x board_x x board_y
         s = F.relu(self.bn3(self.conv3(s)))                          # batch_size x num_channels x (board_x-2) x (board_y-2)
-        # s = F.relu(self.bn4(self.conv4(s)))                          # batch_size x num_channels x (board_x-4) x (board_y-4)
         s = s.view(-1, self.args.num_channels*(self.board_x-2)*(self.board_y-2))
 
         s = F.dropout(F.relu(self.fc_bn1(self.fc1(s))), p=self.args.dropout, training=self.training)  # batch_size x 1024
